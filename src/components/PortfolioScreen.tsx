672308e89df7b9c60ec54d9b385077bf410543c2
parthstark <parthstarky@gmail.com>
2024-07-03 16:30:36 +0530

    setup nativewind

---
+++ src/components/PortfolioScreen.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const PortfolioScreen = () => {
+    return (
+        <View className='flex-1 bg-red-200 items-center justify-center'>
+            <Text>PortfolioScreen</Text>
+        </View>
+    )
+}
+
+export default PortfolioScreen
